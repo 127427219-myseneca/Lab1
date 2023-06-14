b81dcbcd7f6e99c5a941c069344e4bf8e9d23893
vikas-singh-06 <06vikaschauhan@gmail.com>
2023-06-14 17:41:42 -0400

    Updated calculate_age function

--- Lab1.py
+++ Lab1.py
@@ -1,4 +1,8 @@
-def helloWorld():
-    print('Hello World')
+def calculate_age():
+    name = input("Enter your name: ")
+    birth_year = int(input("Enter your birth year: "))
+    current_year = 2023  # Replace with the current year
+    age = current_year - birth_year
+    print(f"{name}, you are {age} years old.")
 
-helloWorld()
+calculate_age()
