adcceca238a4336c5e9b07da10e3b4dfffb19be5
vikas-singh-06 <06vikaschauhan@gmail.com>
2023-06-14 18:02:46 -0400

    Updated HelloWorld finction

--- Lab1.py
+++ Lab1.py
@@ -2,11 +2,3 @@ def helloWorld():
     print('Hello World')
 
 helloWorld()
-def calculate_age():
-    name = input("Enter your name: ")
-    birth_year = int(input("Enter your birth year: "))
-    current_year = 2023  # Replace with the current year
-    age = current_year - birth_year
-    print(f"{name}, you are {age} years old.")
-
-calculate_age()
